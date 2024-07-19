9f157824e44d86993a6c3c6c77a24b87e8350883
ikawlangsapatna143 <bansonchristoper@gmail.com>
2024-07-19 15:43:36 +0800

    Dashboard

--- src/main/java/com/pioneers/medmartbck/repository/ProductBatchRepository.java
+++ src/main/java/com/pioneers/medmartbck/repository/ProductBatchRepository.java
@@ -1,9 +1,12 @@
 package com.pioneers.medmartbck.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pioneers.medmartbck.model.ProductBatch;
 
 public interface ProductBatchRepository extends JpaRepository <ProductBatch, Long> {
 
+    @Query("SELECT SUM(i.quantity) FROM ProductBatch i")
+    Long getTotalProductBatchesCount();
 }
